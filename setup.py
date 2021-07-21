f457cb365e6c0fc126d12a6ccd26032158d003ff
Ivan Svogor <ivan.svogor@iarai.ac.at>
2021-07-21 13:59:23 +0200

    Fix to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="angulargrad",
     version="0.0.1",
     author='S.K. Roy, M.E. Paoletti, J.M. Haut, S.R. Dubey, P. Kar, A. Plaza and B.B. Chaudhuri',
     description='Angulargrad - modified for setup by Ivan Svogor',
     url="https://github.com/isvogor-foi/AngularGrad",
-    package_dir={"angulargrad": "angulargrad"},
-    packages=setuptools.find_packages(where="angulargrad"),
+    package_dir={"": "angulargrad"},
+    packages=[""],
     python_requires=">=3.6",
 )
