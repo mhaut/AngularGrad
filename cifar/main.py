cd8cbeceb8ba729f120c9a0e5cf521c3a8a23bcf
mario <mhaut@users.noreply.github.com>
2021-08-27 14:21:02 +0200

    fix typo

--- cifar/main.py
+++ cifar/main.py
@@ -48,11 +48,11 @@ def get_loaders(dsetname, bsize):
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize(mean, std),
     ])
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        transforms.Normalize(mean, std),
     ])
 
     trainset = torchdset(root='./data/'+dsetname+'/', train=True, download=True, transform=transform_train)
